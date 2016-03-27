f80386205d99fa489ae1653a98baa8bab22db0e3
VISHNU KUMAR NEKKANTI <vishnukumarnekkanti@gmail.com>
2016-03-27 22:24:36 +0530

    dssd

--- src/Admin.py
+++ src/Admin.py
@@ -2,8 +2,10 @@ class CompensationRequest(Request):
 	"""Compensation Request flow"""
 	def __init__(self, component, blood_type, units, compensation_type):
 		super(CompensationRequest, self).__init__( component, blood_type, units, compensation_type)
+		flow()
 		
 	def flow():
+		"""receive compensation, supply, check inventory status"""
 		pass
 
 
@@ -12,6 +14,7 @@ class CompensationRequest(Request):
 class ReplacementRequest(CompensationRequest):
 	"""Replacement Request flow"""
 	def flow():
+		"""record replacer details, replacement details, supply, check inventory status, check for replacement"""
 		pass
 		
 

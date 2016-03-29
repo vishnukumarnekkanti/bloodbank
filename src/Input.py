2a4a1da3967fc72b66479e2d5900e4d09849c05e
VISHNU KUMAR NEKKANTI <vishnukumarnekkanti@gmail.com>
2016-03-29 10:04:03 +0530

    updating input.py

--- src/Input.py
+++ src/Input.py
@@ -1,4 +1,6 @@
 """generates input to the bloodbank"""
+from Admin import *
+import random
 """Daily cycle"""
 #loop1
 #start day 
@@ -8,3 +10,22 @@
 #decide on request or donation
 ###########
 #end day
+def getRand(a,b):
+	return random.randint(a,b)
+
+weighted_choice = lambda s : random.choice(sum(([v]*wt for v,wt in s),[]))
+choices = [(0,99),(1,1)]
+
+def prob():
+    return weighted_choice(choices) 
+
+f = open(filepath, 'r')
+inputData = {}
+for line in f:
+    k, v = line.strip().split(',')
+    inputData[k.strip()] = v.strip()
+
+f.close()
+for day in xrange(1,3651):
+	beginDay()
+	requestNum = (getRand(50,200))*inputData[day] + 3*((getRand(50,200))*inputData[day])*prob()

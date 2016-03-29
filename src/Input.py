e20892b720efb3a3ef5c2d14f71c46dd5d2728bb
VISHNU KUMAR NEKKANTI <vishnukumarnekkanti@gmail.com>
2016-03-29 10:28:01 +0530

    done

--- src/Input.py
+++ src/Input.py
@@ -14,18 +14,37 @@ def getRand(a,b):
 	return random.randint(a,b)
 
 weighted_choice = lambda s : random.choice(sum(([v]*wt for v,wt in s),[]))
-choices = [(0,99),(1,1)]
 
-def prob():
+#choices = [(0,99),(1,1)]
+
+def prob(choices):
     return weighted_choice(choices) 
+if __name__ == '__main__':
+	f = open(filepath, 'r')
+	inputData = {}
+	for line in f:
+	    k, v = line.strip().split(',')
+	    inputData[k.strip()] = v.strip()
 
-f = open(filepath, 'r')
-inputData = {}
-for line in f:
-    k, v = line.strip().split(',')
-    inputData[k.strip()] = v.strip()
-
-f.close()
-for day in xrange(1,3651):
-	beginDay()
-	requestNum = (getRand(50,200))*inputData[day] + 3*((getRand(50,200))*inputData[day])*prob()
+	f.close()
+	component = "rbc"
+	blood_type = "A+"
+	for day in xrange(1,3651):
+		beginDay()
+		choices_0 = [(0,99),(1,1)]
+		requestNum = int((getRand(50,200))*inputData[day] + 3*((getRand(50,200))*inputData[day])*prob(choices_0))
+		donationNum = int((getRand(10,50))*inputData[day])
+		choices_1 = [(0,60),(1,40)]
+		choices_2 = [(0,80),(1,20)]
+		for transaction in xrange(requestNum+donationNum):
+			if prob(choices_1) and donationNum>0:
+				donation(0, component_name, blood_type, (getCurrDate()))
+				donationNum-=1
+			else:
+				units = int((getRand(1,20))*inputData[day] + 3*((getRand(10,20))*inputData[day])*prob(choices_0))
+				if prob(choices_2):
+					compensationRequest(component, blood_type, units)
+				else:
+					ReplacementRequest(component, blood_type, units)
+				requestNum-=1
+		endDay()
